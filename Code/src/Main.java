d54c42a70fc5ca32db444617e49e098efb13d6d5
seza443 <github.tanguy.vaessen@hotmail.com>
2014-12-01 22:30:01 +0100

    init commit :accept:

---
+++ Code/src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main{
+	/**
+	 * Point d'entrée du programme
+	 * @param args
+	 */
+	public static void main(String[] args){
+		System.out.println("Hello François. This is a main =D");
+	}
+}
