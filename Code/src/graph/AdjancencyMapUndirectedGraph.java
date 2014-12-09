6658d8a544f91882ade9852a4753374d6e2f7f9c
bdehem <boris.deh@gmail.com>
2014-12-09 16:02:47 +0100

    AdjacencyMapUndirectedGraph

---
+++ Code/src/graph/AdjancencyMapUndirectedGraph.java
@@ -0,0 +1,92 @@
+package graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class AdjancencyMapUndirectedGraph<V,E> {
+	private class Vertex<V> {
+		private V element;
+		private Map<Vertex<V>,Edge<E>> edges;
+		public Vertex(V elem) {
+			element = elem;
+			edges = new HashMap<Vertex<V>,Edge<E>>();
+		}
+		public V getElement() {
+			return element;
+		}
+		public Map<Vertex<V>, Edge<E>> getEdges() {
+			return edges;
+		}
+	}// end of Vertex class
+	
+	private class Edge<E> {
+		private E element;
+		private Vertex<V>[] endpoints;
+		public Edge(Vertex<V> u, Vertex<V> v, E elem) {
+			element   = elem;
+			endpoints = (Vertex<V>[]) new Vertex[]{u,v};
+		}
+		public E getElement() {
+			return element;
+		}
+		public Vertex<V>[] getEndpoints() {
+			return endpoints;
+		}
+	}// end of Edge class
+	
+	private ArrayList<Vertex<V>> vertices;
+	private ArrayList<Edge<E>> edges;
+	public AdjancencyMapUndirectedGraph(){}
+	public int numVertices() {return vertices.size();}
+	public int numEdges() {return edges.size();}
+	public Iterable<Edge<E>> adjacentEdges(Vertex<V> v) {
+		return v.getEdges().values();
+	}
+	public Edge<E> getEdge(Vertex<V> u, Vertex<V> v){
+		return u.getEdges().get(v);
+	}
+	public Vertex<V>[] endVertices(Edge<E> e){
+		return e.getEndpoints();
+	}
+	
+	public Vertex<V> opposite(Vertex<V> v, Edge<E> e) throws IllegalArgumentException {
+		Vertex<V>[] endPoints = e.getEndpoints();
+		if (endPoints[0] == v)
+			return endPoints[1];
+		else if (endPoints[1] == v)
+			return endPoints[0];
+		else
+			throw new IllegalArgumentException("v is not incident to this edge");
+	}
+
+	public Vertex<V> insertVertex(V element) {
+		Vertex<V> v = new Vertex<>(element);
+		vertices.add(v);
+		return v;
+	}
+	
+	public Edge<E> insertEdge(Vertex<V> u, Vertex<V> v, E element) throws IllegalArgumentException{
+		if (getEdge(u,v)==null) {
+			Edge<E> e = new Edge<>(u,v,element);
+			edges.add(e);
+			u.getEdges().put(v,e);
+			v.getEdges().put(u,e);
+			return e;
+		}else{
+			throw new IllegalArgumentException("Edge between u and v already exists");
+		}
+	}
+	public void removeEdge(Edge<E> e) {
+		e.getEndpoints()[0].getEdges().remove(e);
+		e.getEndpoints()[1].getEdges().remove(e);
+		edges.remove(e);
+	}
+	public void removeVertix(Vertex<V> v) {
+		for (Edge<E> e : v.getEdges().values()){
+			removeEdge(e);
+		}
+		vertices.remove(v);
+	}
+}
