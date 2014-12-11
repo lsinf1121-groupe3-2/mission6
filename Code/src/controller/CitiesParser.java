3cbd5315b37e388fe117d6526273566aeeceefac
seza443 <github.tanguy.vaessen@hotmail.com>
2014-12-11 15:48:36 +0100

    Commentaires

--- Code/src/controller/CitiesParser.java
+++ Code/src/controller/CitiesParser.java
@@ -14,6 +14,16 @@ import java.util.Map;
 
 import controller.exceptions.UncorrectLineException;
 
+/**
+ * Cette classe est utilisée pour convertir un fichier texte contenant des villes et les couts associés pour voyager en avion d'une ville à l'autre (vols directs).
+ * Le format de ce fichier doit être le suivant:
+ * ville1	ville2	cout
+ * ville3	ville4	cout
+ * 
+ * Ou chacun des éléments est un entier positif.
+ * @author Tanguy
+ *
+ */
 public class CitiesParser {
 	BufferedReader br;
 	AdjancencyMapUndirectedGraph<Integer,Integer> graph;
@@ -24,6 +34,18 @@ public class CitiesParser {
 		alreadyAddedVertices = new HashMap<>();
 	}
 	
+	/**
+	 * @pre citiesFilePath est un chemin de fichier valide
+	 * @post Le fichier citiesFilePath a été interprété en un graphe de type AdjancencyMapUndirectedGraph<Integer,Integer>.
+	 * Chaque ville présente dans le fichier a été ajoutée une seule fois au graphe
+	 * Chaque lien représenté par une ligne du fichier a été ajoutée au graphe.
+	 * Le graphe est retourné.
+	 * 
+	 * @throws UncorrectLineException Exception lancée si une ligne du fichier citiesFilePath est mal formée.
+	 * @throws FileNotFoundException Exception lancée si le fichier citiesFilePath n'a pu être trouvé.
+	 * @throws IOException Exception lancée si une erreur d'ouverture, de lecture ou de fermeture du fichier survient.
+	 * @throws IllegalArgumentException Exception lancée si un lien entre 2 villes est ajouté 2 fois.
+	 */
 	public AdjancencyMapUndirectedGraph<Integer,Integer> parseCitiesFile(String citiesFilePath) throws UncorrectLineException, FileNotFoundException, IOException, IllegalArgumentException{
 		this.initializeReader(citiesFilePath);
 		
@@ -44,6 +66,18 @@ public class CitiesParser {
 		return graph;
 	}
 
+	/**
+	 * @pre La variable graph est initialisée. Elle représente un graphe vide, non null.
+	 * La Map alreadyAddedVertices est initialisée.
+	 * L'arguments args contient 3 entrées.
+	 * Entrée 1 = la ville 1
+	 * Entrée 2 = la ville 2
+	 * Entrée 3 = le cout associé au vol direct entre ville 1 et ville 2
+	 * @post Ville 1 et ville 2 sont ajoutés au graphe s'ils n'étaient aps encoré présent dedans.
+	 * Le lien entre les 2 villes est ajouté au graphe.
+
+	 * @throws IllegalArgumentException Exception lancée si un lien entre 2 villes est déjà présent dans le graphe.
+	 */
 	private void parseLine(String[] args) throws IllegalArgumentException {
 		Integer city1 = Integer.parseInt(args[0]);
 		Integer city2 = Integer.parseInt(args[1]);
