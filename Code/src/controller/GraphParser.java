154e2169dbb69671503b63f5d4a9e927baf18b8c
JoPowell <john_sib@hotmail.com>
2014-12-12 15:42:42 +0100

    tatal

--- Code/src/controller/GraphParser.java
+++ Code/src/controller/GraphParser.java
@@ -15,10 +15,27 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/**
+ * Cette classe est utilisée pour convertir un graphe contenant des villes et les couts associés pour voyager en avion d'une ville à l'autre (vols directs).
+ * Le format de sortie du fichier text est le suivant:
+ * ville1	ville2	cout
+ * ville3	ville4	cout
+ * 
+ * Ou chacun des éléments est un entier positif.
+ * 
+ * @author Alexandre, Jonathan
+ *
+ */
+
 public class GraphParser {
 	private BufferedWriter output;
 	private AdjacencyMapUndirectedGraph<Integer, Integer> graph;
 	
+	
+	/**
+	 *
+	 * @throws IOException Exception si c'est impossible d'ouvrir le fichier.
+	 */
 	public GraphParser(File file, AdjacencyMapUndirectedGraph<Integer, Integer> graph) {
 		try {
 			this.output = new BufferedWriter(new FileWriter(file));
